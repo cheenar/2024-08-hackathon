74da02b5f840f59c65d4d4a9d580300a1409782e
cheenar <cheenar@hey.com>
2024-08-24 20:04:26 -0700

    changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import { Provider } from "react-redux";
-import store from "./store.ts";
 import ClaudeInterface from "./App.tsx";
+import "./index.css";
+import store from "./store.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
